57d52978b99920aefdcd5672ff140ea25596cb0c
Александр Хохлачёв <demcy.meizu@gmail.com>
2021-02-04 21:59:33 +0200

    initial commit

---
+++ src/components/shared/Header.tsx
@@ -0,0 +1,8 @@
+export const Header = () =>
+    <ul>
+        <li><a className="active" href="#home">Home</a></li>
+        <li><a href="#news">News</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li style={{float:"right"}}><a href="#about">About</a></li>
+    </ul>
+
